60b20e5e09cc7c18b1b0519b892256d2672e93c1
Tyler Gary <tygary@gmail.com>
2020-06-04 12:26:42 -0700

    updating assignment to remove statements

--- global.js
+++ global.js
@@ -1,50 +1,42 @@
-
 // Welcome! Here's an example of a console logged response
-console.log(4 + 5)
+console.log(4 + 5);
 
 /***********************/
-/* Part 1: Expressions */
+/* Using Expressions   */
 /***********************/
 
 //1.) Log an expression that evaluates to the sum of two integers
-// your code here 
-console.log('answer 1');
+// your code here
+console.log("answer 1");
 
 // 2.) log an expression that evaluates to the difference of two integers
-// your code here 
-console.log('answer 2');
+// your code here
+console.log("answer 2");
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
-// your code here 
-console.log('answer 3');
+// your code here
+console.log("answer 3");
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
-// your code here 
-console.log('answer 4');
+// your code here
+console.log("answer 4");
 
 // 5.) Log an expression that uses modulo, with a result of 4
 // your code here
-console.log('answer 5');
+console.log("answer 5");
 
-/**********************/
-/* Part 2: Statements */
-/**********************/
+// 6.) Create two different strings and compare them, logging the result.
+// It should be false!
+console.log("answer 6");
 
-// 6.) Assign a variable and console log the declaration
-// your code here 
-console.log('answer 6');
+// 7.) Concatenate 4 different strings to form the sentence "I'm learning a bit of JavaScript at Kenzie"
+console.log("answer 7");
 
-// 7.) Console log a variable assigned to the sum of two numbers
-// your code here
-console.log('answer 8');
+// 8.) Uncomment this code and create an expression inside of it.
+// The expression should represent: 5 is less than 10 and 8 is greater than 5
+// This should print true
 
-// 8.) Uncomment this if else statement and add a logical expression as the condition
-//if (/*your code here*/) {
-//    console.log('It was!')
-//} else {
-//    console.log('It was not..')
-//}
+// console.log("answer 8")
 
 // 9.) Add parenthesis to the following statement, the result should be 1
-let answer = 3 * 3 + 1 / 8 + 12 / 2;
-console.log(answer == 1); // This should log true
+console.log(3 * 3 + 1 / 20 / 2 == 1); // This should log true
