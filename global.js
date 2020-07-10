e5135a653c2eee8d8bafd54c6e2e363abaa08277
peachykish <nakisha.carroll@gmail.com>
2020-07-10 11:59:24 -0400

    additional commit

--- global.js
+++ global.js
@@ -7,19 +7,19 @@ console.log(4 + 5);
 
 //1.) Log an expression that evaluates to the sum of two integers
 // your code here
-console.log("answer 1");
+console.log("1 + 2");
 
 // 2.) log an expression that evaluates to the difference of two integers
 // your code here
-console.log("answer 2");
+console.log("2 - 1");
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
 // your code here
-console.log("answer 3");
+console.log("I'm writing" + "my first JavaScript code");
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
 // your code here
-console.log("answer 4");
+console.log("12 > 15") //returns false;
 
 // 5.) Log an expression that uses modulo, with a result of 4
 // your code here
@@ -30,13 +30,13 @@ console.log("answer 5");
 console.log("answer 6");
 
 // 7.) Concatenate 4 different strings to form the sentence "I'm learning a bit of JavaScript at Kenzie"
-console.log("answer 7");
+console.log("I'm learning" + "a bit" + "of JavaScript" + "at Kenzie");
 
-// 8.) Uncomment this code and create an expression inside of it.
-// The expression should represent: 5 is less than 10 and 8 is greater than 5
-// This should print true
+8.) Uncomment this code and create an expression inside of it.
+The expression should represent: 5 is less than 10 and 8 is greater than 5
+This should print true
 
-// console.log("answer 8")
+console.log("5 < 10" + 8 > 5)
 
 // 9.) Add parenthesis to the following statement, the result should be 1
-console.log(3 * 3 + 1 / 20 / 2 == 1); // This should log true
+console.log(3 * 3 + 1 / (20 / 2) == 1); // This should log true
